7311858c617d486063d5a38e4014a7ba3499c9d0
Dom Denis <114570732+Domdenis@users.noreply.github.com>
2024-01-14 16:22:35 +0100

    Update ergotech.py

--- ergotech.py
+++ ergotech.py
@@ -69,7 +69,7 @@ response_container = st.container()
 # Gestion de la saisie utilisateur
 with st.form(key='my_form', clear_on_submit=True):
     user_input = st.text_area("You:", key='input', height=100)
-    submit_button = st.form_submit_button(label='Send')
+    submit_button = st.form_submit_button(label='Analyser et rechercher')
 
     if submit_button and user_input:
         output, total_tokens, prompt_tokens, completion_tokens, cost = generate_response(user_input)
@@ -83,8 +83,8 @@ with st.form(key='my_form', clear_on_submit=True):
 if st.session_state['generated']:
     with response_container:
         for i in range(len(st.session_state['generated'])):
-            message(st.session_state["past"][i], is_user=True, key=str(i) + '_user')
-            message(st.session_state["generated"][i], key=str(i))
+            message(st.session_state["past"][i], is_user=True, avatar_style="aventurer", key=str(i) + '_user')
+            message(st.session_state["generated"][i], is_user=False, avatar_style="Identicon", key=str(i))
             st.write(f"Number of tokens: {st.session_state['total_tokens'][i]}; Cost: ${st.session_state['cost'][i]:.5f}")
 
 # Bouton "Continuer" (optionnel)
